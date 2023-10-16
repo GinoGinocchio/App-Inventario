253832dfe36aea21bed1ac823e3897a001ab8ddd
GinoGinocchio <gino.ginocchio@unmsm.edu.pe>
2023-10-15 21:44:53 -0500

    Patrón de repositorio

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using AppInventario.AccesoDatos.Data;
+using AppInventario.AccesoDatos.Repositorio;
+using AppInventario.AccesoDatos.Repositorio.IRepositorio;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +15,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+builder.Services.AddScoped<IUnidadTrabajo, UnidadTrabajo>();
 
 var app = builder.Build();
 
