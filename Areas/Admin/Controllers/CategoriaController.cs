da20860c7acd1afad235555838a74eedc746059d
GinoGinocchio <gino.ginocchio@unmsm.edu.pe>
2023-11-19 19:26:37 -0500

    Crud de Categoria

---
+++ Areas/Admin/Controllers/CategoriaController.cs
@@ -0,0 +1,108 @@
+﻿using AppInventario.AccesoDatos.Repositorio;
+using AppInventario.AccesoDatos.Repositorio.IRepositorio;
+using AppInventario.Modelos;
+using AppInventario.Utilidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppInventario.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriaController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public CategoriaController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task <IActionResult> Upsert(int? id)
+        {
+            Categoria categoria = new Categoria();
+            if (id == null)
+            {
+                //Crear nueva Bodega
+                categoria.Estado = true;
+                return View(categoria);
+            }
+            categoria = await _unidadTrabajo.Categoria.Obtener(id.GetValueOrDefault());
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Categoria categoria)
+        {
+            if(ModelState.IsValid)
+            {
+                if (categoria.Id == 0)
+                {
+                    await _unidadTrabajo.Categoria.Agregar(categoria);
+                    TempData[DS.Exitosa] = "Categoría creada exitosamente";
+                }
+                else
+                {
+                    _unidadTrabajo.Categoria.Actualizar(categoria);
+                    TempData[DS.Exitosa] = "Categoría actualizada exitosamente";
+                }
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index));
+            }
+            TempData[DS.Error] = "Error al grabar Categoría";
+            return View(categoria);
+        }
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Categoria.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var categoriaDB = await _unidadTrabajo.Categoria.Obtener(id);
+            if(categoriaDB == null)
+            {
+                return Json(new { success = false, message = "Error al borrar Categoría" });
+            }
+            _unidadTrabajo.Categoria.Remover(categoriaDB);
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Categoria borrada exitosamente" });
+        }
+
+        [ActionName("ValidarNombre")]
+        public async Task<IActionResult> ValidarNombre(string nombre, int id = 0)
+        {
+            bool valor = false;
+            var lista = await _unidadTrabajo.Categoria.ObtenerTodos();
+            if( id == 0)
+            {
+                valor = lista.Any(x => x.Nombre.ToLower().Trim() == nombre.ToLower().Trim());
+            }
+            else
+            {
+                valor = lista.Any(x => x.Nombre.ToLower().Trim() == nombre.ToLower().Trim() && x.Id != id);
+            }
+            if(valor)
+            {
+                return Json(new { data = true });
+            }
+            return Json(new { data = false });
+        }
+
+
+        #endregion
+    }
+}
