e6024f6307b1b37ce249ac2f4c12bb0e617e1464
GinoGinocchio <gino.ginocchio@unmsm.edu.pe>
2024-01-15 23:35:00 -0500

    Diseño Pagina principal

--- Areas/Inventario/Controllers/HomeController.cs
+++ Areas/Inventario/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using AppInventario.Modelos.ViewModels;
+﻿using AppInventario.AccesoDatos.Repositorio.IRepositorio;
+using AppInventario.Modelos;
+using AppInventario.Modelos.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +10,17 @@ namespace AppInventario.Areas.Inventario.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        public HomeController(ILogger<HomeController> logger, IUnidadTrabajo unidadTrabajo)
         {
             _logger = logger;
+            _unidadTrabajo = unidadTrabajo;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IEnumerable<Producto> productolista = await _unidadTrabajo.Producto.ObtenerTodos();
+            return View(productolista);
         }
 
         public IActionResult Privacy()
